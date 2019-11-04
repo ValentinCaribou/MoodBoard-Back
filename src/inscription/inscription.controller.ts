303f81fd28cc32b5cb2e91f631a8d6e3044cc888
Valentin BURGAUD <valentin.burgaud@sii.fr>
2019-11-04 16:45:09 +0100

    Ajout de la methode Update pour l'utilisateur

--- src/inscription/inscription.controller.ts
+++ src/inscription/inscription.controller.ts
@@ -1,4 +1,4 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
+import {Body, Controller, Get, Param, Post, Put} from '@nestjs/common';
 import {LoginService} from "../login/login.service";
 import {InscriptionService} from "./inscription.service";
 
@@ -14,5 +14,10 @@ export class InscriptionController {
     @Post()
     async inscription(@Body() body) {
         return await this.inscriptionService.create(body);
+    }
+
+    @Put(':id')
+    async update(@Param('id') id, @Body() body){
+        return await this.inscriptionService.update(id, body);
     }
 }
