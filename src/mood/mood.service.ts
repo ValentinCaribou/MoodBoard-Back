ad359d0c4b3c596dfc01cba324ba679ed4faa547
Valentin BURGAUD <valentin.burgaud@sii.fr>
2019-12-04 10:20:02 +0100

    Ajout du PROJEEEEEETTTTTTTT

--- src/mood/mood.service.ts
+++ src/mood/mood.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import {InjectModel} from "@nestjs/mongoose";
 import {Mood} from "../interfaces/mood.interface";
-import { Model } from 'mongoose';
+import {Model} from 'mongoose';
 import {CreateMoodDto} from "../dto/createMood.dto";
 
 @Injectable()
